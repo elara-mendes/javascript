71cd1ac6f3ed460ca20b039e55724692c8fb43cd
elara-mendes <elaramendes@yahoo.com>
2025-01-31 21:57:12 -0300

    Upload exercises

---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,9 @@
+let title = document.querySelector("h1");
+title.innerHTML = "Jogo do Número Secreto";
+
+let paragraph = document.querySelector("p");
+paragraph.innerHTML = "Escolha um número entre 1 e 10";
+
+function verificarChute() {
+    console.log("O botão foi clicado.")
+}

3a9de4f05dfc7070ed1da38b58a9301f2799c46e
elara-mendes <elaramendes@yahoo.com>
2025-02-02 19:01:21 -0300

    Upload exercises

--- exercises/main.js
+++ exercises/main.js
@@ -1,33 +1,90 @@
-function helloClick() {
-    console.log("Hello World!");
+// function helloClick() {
+//     console.log("Hello World!");
+// }
+
+// function nameClick(nome) {
+//     console.log(`Olá ${nome}`);
+// }
+
+// nameClick(prompt("Escreva um nome:"));
+
+// function numberDouble(number) {
+//     console.log(number * 2);
+// }
+
+// numberDouble(prompt("Digite um número:"));
+
+// function numberMedia(n1, n2, n3) {
+//     console.log((n1 + n2 + n3) / 3);
+// }
+
+// numberMedia(4, 5, 6)
+
+// function maxNumber(n1, n2) {
+//     console.log(Math.max(n1, n2));
+// }
+
+// maxNumber(5, 10);
+
+// function multiNumber(n1) {
+//     console.log(n1*n1);
+// }
+
+// multiNumber(2);
+
+function calculateIMC(altura, peso) {
+    let alturaCm = altura / 100;
+    resultado = peso / alturaCm**2;
+    console.log(`O IMC é ${resultado.toFixed(2)}`);
 }
 
-function nameClick(nome) {
-    console.log(`Olá ${nome}`);
+calculateIMC(180, 102);
+
+function factorialFun(numero) {
+    valor = 1;
+    contador = 1;
+    while (contador <= numero) {
+        valor *= contador;
+        contador++;
+    }
+    console.log(`O fatorial de ${numero} é ${valor}`);
 }
 
-nameClick(prompt("Escreva um nome:"));
+factorialFun(5);
 
-function numberDouble(number) {
-    console.log(number * 2);
+function dollarToReal(dollar) {
+    valorDollar = dollar;
+    console.log(`O valor em dólar é ${parseFloat(valorDollar*4.80)}`);
 }
 
-numberDouble(prompt("Digite um número:"));
+dollarToReal(5);
 
-function numberMedia(n1, n2, n3) {
-    console.log((n1 + n2 + n3) / 3);
+function areaPerimetro(largura, altura) {
+    area = largura * altura;
+    console.log(`A área é ${parseFloat(area)}`);
+    perimetro = 2 * (largura + altura);
+    console.log(`O perímetro é ${parseFloat(perimetro)}`);
 }
 
-numberMedia(4, 5, 6)
+areaPerimetro(5, 3);
 
-function maxNumber(n1, n2) {
-    console.log(Math.max(n1, n2));
+function raioSala(raio) {
+    pi = 3.14;
+    area = parseFloat(pi * raio**2);
+    console.log(area);
+    perimetro = parseFloat(2 * pi * raio);
+    console.log(perimetro.toFixed(1));
 }
 
-maxNumber(5, 10);
+raioSala(5)
 
-function multiNumber(n1) {
-    console.log(n1*n1);
+function tabelaTabuada(numero) {
+    contador = 1;
+    while (contador < 11) {
+        resultado = numero * contador;
+        console.log(`${numero} x ${contador} = ${resultado}`);
+        contador ++;
+    }
 }
 
-multiNumber(2);
+tabelaTabuada(2)

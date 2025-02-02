f31bfee96d7c8520f74b0638285e734fab165b1c
elara-mendes <elaramendes@yahoo.com>
2025-02-02 16:20:24 -0300

    Upload lesson exercises

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -4,10 +4,14 @@
 // let paragraph = document.querySelector("p");
 // paragraph.innerHTML = "Escolha um número entre 1 e 10";
 
+// let secretNumber = randomNumber();
 let secretNumber = randomNumber();
+let tentativas = 1;
+let reiniciarBotao = document.getElementById("reiniciar");
+let chutarBotao = document.getElementById("chutar");
+console.log(secretNumber);
 
-showText('h1', 'Jogo do Número Secreto');
-showText('p', 'Escolha um número entre 1 e 10'); 
+mensagemInicial();
 
 function showText(tag, text) {
     let Element = document.querySelector(tag);
@@ -19,6 +23,38 @@ function randomNumber() {
 }
 
 function verificarChute() {
-    kick = document.querySelector('input').value;
-    console.log(kick == secretNumber);
+    let kick = document.querySelector('input').value;
+    if (kick == secretNumber) {
+        showText("h1", "Você acertou!");
+        let palavraTentativa = (tentativas > 1) ? "tentativas" : "tentativa";
+        showText("p", `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`);
+        reiniciarBotao.removeAttribute("disabled");
+        chutarBotao.setAttribute("disabled", "true");
+    } else if (kick > secretNumber) {
+        showText("p", "O número é menor!");
+    } else {
+        showText("p", "O número é maior!");
+    }
+    tentativas ++;
+    limparCampo();
+    // console.log(kick == secretNumber);
+}
+
+function limparCampo() {
+    kick = document.querySelector('input');
+    kick.value = "";
+}
+
+function reiniciarJogo() {
+    secretNumber = randomNumber();
+    limparCampo();
+    tentativas = 1;
+    reiniciarBotao.setAttribute('disabled', "true");
+    chutarBotao.removeAttribute("disabled");
+    mensagemInicial();
+}
+
+function mensagemInicial() {
+    showText('h1', 'Jogo do Número Secreto');
+    showText('p', 'Escolha um número entre 1 e 10'); 
 }
